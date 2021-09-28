08c0b2bc79e5495afbcbb0ba408d71ab1f15adf1
EudesSerpa <serpagame@gmail.com>
2021-09-28 15:48:08 -0500

    Service Page Finished with Scroll Efects

---
+++ assets/services.js
@@ -0,0 +1,18 @@
+const cardsAnimated = document.querySelectorAll('.animationCard');
+
+function showCards() {
+    cardsAnimated.forEach(card => {
+        //Posicion, relativa al viewport, de las cards
+        const cardPosition = card.getBoundingClientRect().top;
+
+        //Altura de la ventana
+        const viewportHeight = window.innerHeight / 2;
+
+        if(cardPosition < viewportHeight){
+            card.style.opacity = 1;
+            card.classList.add('showCardFromLeft');
+        }
+    });
+}
+
+window.addEventListener('scroll', showCards);
