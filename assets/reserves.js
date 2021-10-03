32a4d34b72bce23e3cebc0ab4a186a69724cf919
EudesSerpa <serpagame@gmail.com>
2021-10-02 19:27:58 -0500

    Joined validations and send email

--- assets/reserves.js
+++ assets/reserves.js
@@ -1,126 +1,83 @@
-const form = document.getElementById('form');
-const buttonSubmitForm = document.getElementById('submit');
+import { fieldsToValidate, labelsStyled } from "./validations.js";
 
-let templateModal = ''
+const form = document.getElementById('form');
+const textFormInvalid = document.querySelector('.form-invalid--text');
+const buttonSubmitForm = document.getElementById('submit');
+const modalBody = document.querySelector('.modal-body');
+
+const templateModalOk = `
+<p>Tu reserva se ha realizado éxitosamente. 📅</p>
+<p>Gracias por preferirnos. 💖</p>
+<br>
+<p>Sal&Salsa Restaurant © JuvenTIC.</p>
+`;
+
+const templateModalError = `
+<p>Lo sentimos, hubo un problema al enviar la reserva de tu servicio. ❌</p>
+<p>Estamos trabajando en ello. 🛠</p>
+<p>Si el problema persiste, te invitamos a comunicarte por otro medio.</p>
+<p>Agradecemos tu comprensión.</p>
+<br>
+<p>Sal&Salsa Restaurant © JuvenTIC.</p>
+`;
 
 async function handleSubmit(event) {
     event.preventDefault();
 
-    const formData = new FormData(this);
+    const validations = Object.values(fieldsToValidate);
 
-    const response = await fetch(this.action, {
-        method: this.method,
-        body: formData,
-        headers: {
-            'Accept': 'application/json'
-        },
-    });
+    if(validations.every(validation => validation)) {
+        //Enviar
+        const formData = new FormData(this);
 
-    if(response.ok) {
-        this.reset();
-        templateModal = `
-            <div class="modal fade" id="modalAlert" tabindex="-1" aria-labelledby="modalAlertLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <p class="modal-title" id="modalAlertLabel">Reservación</p>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <p>Tu reserva se ha realizado éxitosamente. 📅</p>
-                            <p>Gracias por preferirnos. 💖</p>
-                            <br>
-                            <p>Sal&Salsa Restaurant © JuvenTIC.</p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn" data-bs-dismiss="modal">Ok!</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
-        renderModal(document.getElementById('containerModal'));
+        try {
+            const response = await fetch(this.action, {
+                method: this.method,
+                body: formData,
+                headers: {
+                    'Accept': 'application/json'
+                },
+            });
+        } catch (error) {
+            console.log('Error: No se pudo enviar el email', error);
+
+            alert('Lo sentimos pero su reserva no se pudo enviar. Por favor, revise su conexión a internet o comuniquese con nosotros por otro medio. Agradecemos su comprensión.');
+        }
+
+        /*Problemas: 
+        1. Despues de renderizado el modal, el campo de nombre se vuelve false, por el evento blur: pierde el focus. Por lo que se activan los estilos para campos invalidos.
+        2. Si el if para la respuesta es false, si no se resetea el formulario, se renderiza y se envia, una y otra vez, el modal y el formulario. Creo que es por el method click que simula el click y se propaga siempre. Esto mismo sucede si se renderiza el modal en la clausula catch, por eso mientras tanto, se puso el alert.
+        */
+        if(response.ok) {
+            this.reset();
+            renderModal(modalBody, templateModalOk);
+        }else {
+            console.log('Error');
+            // renderModal(modalBody, templateModalError);
+        }
+
+        labelsStyled.forEach(field => {
+            field.classList.remove('valid');
+        });
     }else {
-        templateModal = `
-            <div class="modal fade" id="modalAlert" tabindex="-1" aria-labelledby="modalAlertLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <p class="modal-title" id="modalAlertLabel">Reservación</p>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <p>Lo sentimos, hubo un problema al enviar la reserva de tu servicio. ❌</p>
-                            <p>Estamos trabajando en ello. 🛠</p>
-                            <br>
-                            <p>Sal&Salsa Restaurant © JuvenTIC.</p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn" data-bs-dismiss="modal">Ok!</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
-        renderModal(document.getElementById('containerModal'));
-    }
+        //No enviar
+        textFormInvalid.classList.remove('no-show');
+        textFormInvalid.classList.add('show');
 
-    if(false) {
-        this.reset();
-        templateModal = `
-            <div class="modal fade" id="modalAlert" tabindex="-1" aria-labelledby="modalAlertLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <p class="modal-title" id="modalAlertLabel">Reservación</p>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <p>Tu reserva se ha realizado éxitosamente. 📅</p>
-                            <p>Gracias por preferirnos. 💖</p>
-                            <br>
-                            <p>Sal&Salsa Restaurant © JuvenTIC.</p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn" data-bs-dismiss="modal">Ok!</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
-        renderModal(document.getElementById('containerModal'));
-    }else {
-        templateModal = `
-            <div class="modal fade" id="modalAlert" tabindex="-1" aria-labelledby="modalAlertLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <p class="modal-title" id="modalAlertLabel">Reservación</p>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <p>Lo sentimos, hubo un problema al enviar la reserva de tu servicio. ❌</p>
-                            <p>Estamos trabajando en ello. 🛠</p>
-                            <br>
-                            <p>Sal&Salsa Restaurant © JuvenTIC.</p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn" data-bs-dismiss="modal">Ok!</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
-        renderModal(document.getElementById('containerModal'));
+        setTimeout(() => {
+            textFormInvalid.classList.remove('show');
+            textFormInvalid.classList.add('no-show');
+        }, 3000);
     }
 }
 
-function renderModal(node) {
+
+function renderModal(node, modal) {
     // Atibutos de Bootstrap para el modal
-    buttonSubmitForm.setAttribute('data-bs-target', '#modalAlert');
+    buttonSubmitForm.setAttribute('data-bs-target', '#modal-container');
     buttonSubmitForm.setAttribute('data-bs-toggle', 'modal');
 
-    node.innerHTML = templateModal;
+    node.innerHTML = modal;
 
     //Click para activar el modal despupes de agg los atributos de Bootstrap
     buttonSubmitForm.click();
--- assets/validations.js
+++ assets/validations.js
@@ -1,34 +1,47 @@
-const form = document.getElementById('form');
-const textFormInvalid = document.querySelector('.form-invalid--text');
+// const form = document.getElementById('form');
+// const textFormInvalid = document.querySelector('.form-invalid--text');
 const inputs = document.querySelectorAll('.form-inputToValidate');
 const selectService = document.getElementById('service');
 
-const stylishFields = []; //Almacenar nodos Labels - Para quitar estilos de validacion
+const fieldsToValidate = {};
+const labelsStyled = [];
+const regex = {
+    regexName: /^([a-z]+)\s[a-z]+(\s[a-z]+|\s[a-z]+\s[a-z]+|\s[a-z]+\s[a-z]+\s[a-z]+)?$/,
+    regexPhone: /^[3]\d{9}$/,
+    regexAmountPeople: /^[1-9]\d*$/,
+    regexEmail: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    //Hay que formatear la fecha para que cumpla con estos estilos ya que el
+        //datetime-local el formato es: AAAA-MM-DDTHH:MM
+    // match: 11/24/0004 11:59 PM | 2.29.2008 | 02:50:10
+    // no match: 12/33/1020 | 2/29/2005 | 13:00 AM
+    // regexDateOfReserve: /(?=\d)^(?:(?!(?:10\D(?:0?[5-9]|1[0-4])\D(?:1582))|(?:0?9\D(?:0?[3-9]|1[0-3])\D(?:1752)))((?:0?[13578]|1[02])|(?:0?[469]|11)(?!\/31)(?!-31)(?!\.31)|(?:0?2(?=.?(?:(?:29.(?!000[04]|(?:(?:1[^0-6]|[2468][^048]|[3579][^26])00))(?:(?:(?:\d\d)(?:[02468][048]|[13579][26])(?!\x20BC))|(?:00(?:42|3[0369]|2[147]|1[258]|09)\x20BC))))))|(?:0?2(?=.(?:(?:\d\D)|(?:[01]\d)|(?:2[0-8])))))([-.\/])(0?[1-9]|[12]\d|3[01])\2(?!0000)((?=(?:00(?:4[0-5]|[0-3]?\d)\x20BC)|(?:\d{4}(?!\x20BC)))\d{4}(?:\x20BC)?)(?:$|(?=\x20\d)\x20))?((?:(?:0?[1-9]|1[012])(?::[0-5]\d){0,2}(?:\x20[aApP][mM]))|(?:[01]\d|2[0-3])(?::[0-5]\d){1,2})?$/,
+};
 
 
-function handleSubmit(event) {
-    event.preventDefault();
 
-    const validations = Object.values(fieldsToValidate);
+// function handleSubmit(event) {
+//     event.preventDefault();
 
-    if(validations.every(validation => validation) && selectService.value) {
-        //Enviar
-        this.reset();
+//     const validations = Object.values(fieldsToValidate);
 
-        stylishFields.forEach(field => {
-            field.classList.remove('valid');
-        });
-    }else {
-        //No enviar
-        textFormInvalid.classList.remove('no-show');
-        textFormInvalid.classList.add('show');
+//     if(validations.every(validation => validation)) {
+//         //Enviar
+//         this.reset();
 
-        setTimeout(() => {
-            textFormInvalid.classList.remove('show');
-            textFormInvalid.classList.add('no-show');
-        }, 3000);
-    }
-}
+//         labelsStyled.forEach(field => {
+//             field.classList.remove('valid');
+//         });
+//     }else {
+//         //No enviar
+//         textFormInvalid.classList.remove('no-show');
+//         textFormInvalid.classList.add('show');
+
+//         setTimeout(() => {
+//             textFormInvalid.classList.remove('show');
+//             textFormInvalid.classList.add('no-show');
+//         }, 3000);
+//     }
+// }
 
 
 function handleFields(event) {
@@ -36,33 +49,34 @@ function handleFields(event) {
 
     switch(event.target.name) {
         case 'name':
-            validateField(regex.regexName, data, 'name');
+            validateInput(regex.regexName, data.toLowerCase(), 'name');
             break;
         case 'email':
-            validateField(regex.regexEmail, data, 'email');
+            validateInput(regex.regexEmail, data, 'email');
             break;
         case 'phone':
-            validateField(regex.regexPhone, data, 'phone');
+            validateInput(regex.regexPhone, data, 'phone');
             break;
         case 'amountPeople':
-            validateField(regex.regexAmountPeople, data, 'amountPeople');
+            validateInput(regex.regexAmountPeople, data, 'amountPeople');
             break;
         // case 'dateOfReserve':
-        //     validateField(regex.regexDateOfReserve, data, 'dateOfReserve');
+        //     validateInput(regex.regexDateOfReserve, data, 'dateOfReserve');
         //     break;
     };
 };
 
 
-function validateField(regExp, data, field) {
+function validateInput(regExp, data, field) {
     let labelToStyle =  document.querySelector(`.input--${field}`).nextElementSibling
     let textError = document.querySelector(`.text-error--${field}`);
 
+    labelsStyled.push(labelToStyle);
+
     if(field === 'amountPeople') {
+        //Tiene un maquetado diferente (label esta antes del input)
         labelToStyle =  document.querySelector(`.input--${field}`).previousElementSibling;
     }
-
-    stylishFields.push(labelToStyle);
 
     if(regExp.test(data)){
         labelToStyle.classList.remove('invalid');
@@ -84,32 +98,18 @@ function validateField(regExp, data, field) {
 }
 
 
+function validateSelect(event) {
+    fieldsToValidate.service = event.target.value ? true : false;
+}
 
-form.addEventListener('submit', handleSubmit);
 
+// form.addEventListener('submit', handleSubmit);
 inputs.forEach(input => {
     input.addEventListener('keyup', handleFields);
     input.addEventListener('blur', handleFields);
 });
+selectService.addEventListener('blur', validateSelect);
 
 
 
-const fieldsToValidate = {
-    name: false,
-    email: false,
-    phone: false,
-    amountPeople: true,
-    // dateOfReserve: true,
-};
-
-const regex = {
-    regexName: /^([a-z]+)\s[a-z]+(\s[a-z]+|\s[a-z]+\s[a-z]+|\s[a-z]+\s[a-z]+\s[a-z]+)?$/,
-    regexPhone: /^[3]\d{9}$/,
-    regexAmountPeople: /^[1-9]\d*$/,
-    regexEmail: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    //Hay que formatear la fecha para que cumpla con estos estilos ya que el
-        //datetime-local el formato es: AAAA-MM-DDTHH:MM
-    // match: 11/24/0004 11:59 PM | 2.29.2008 | 02:50:10
-    // no match: 12/33/1020 | 2/29/2005 | 13:00 AM
-    // regexDateOfReserve: /(?=\d)^(?:(?!(?:10\D(?:0?[5-9]|1[0-4])\D(?:1582))|(?:0?9\D(?:0?[3-9]|1[0-3])\D(?:1752)))((?:0?[13578]|1[02])|(?:0?[469]|11)(?!\/31)(?!-31)(?!\.31)|(?:0?2(?=.?(?:(?:29.(?!000[04]|(?:(?:1[^0-6]|[2468][^048]|[3579][^26])00))(?:(?:(?:\d\d)(?:[02468][048]|[13579][26])(?!\x20BC))|(?:00(?:42|3[0369]|2[147]|1[258]|09)\x20BC))))))|(?:0?2(?=.(?:(?:\d\D)|(?:[01]\d)|(?:2[0-8])))))([-.\/])(0?[1-9]|[12]\d|3[01])\2(?!0000)((?=(?:00(?:4[0-5]|[0-3]?\d)\x20BC)|(?:\d{4}(?!\x20BC)))\d{4}(?:\x20BC)?)(?:$|(?=\x20\d)\x20))?((?:(?:0?[1-9]|1[012])(?::[0-5]\d){0,2}(?:\x20[aApP][mM]))|(?:[01]\d|2[0-3])(?::[0-5]\d){1,2})?$/,
-};
+export { fieldsToValidate, labelsStyled };
